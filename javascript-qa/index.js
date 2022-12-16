8756fa6a61b322a098bab7af8cb13649275ef201
Susheel Kumar <developer.susheel@gmail.com>
2022-12-16 10:49:49 +0530

    JS QA

---
+++ javascript-qa/index.js
@@ -0,0 +1,34 @@
+// Callback Fucntion: A function that passed as an argument to be call back later
+// function myQuote(quote, callback){
+//   var quote = 'Tis the '+ quote;
+//   callback(quote); 
+// }
+
+// function newQuote(data){
+//   console.log(data);
+// }
+
+// myQuote('Season', newQuote);
+
+
+// Reverse the string and each word in the sentence
+// let string = 'Welcome to Impetus';
+// const reverseEntireSentence = reverse(string, "");
+// const reverseEntireSentenceByWords = reverse(reverseEntireSentence, " ");
+
+// function reverse(string, separator){
+//   return string.split(separator).reverse().join(separator);
+// }
+
+// console.log(reverseEntireSentence);
+// console.log(reverseEntireSentenceByWords);
+
+// Check if an object is an array or not
+// var arrayList = [1, 2, 3, 4, 5, 6, 7, 8]
+// if(Object.prototype.toString.call(arrayList) === '[object Array]'){
+//   console.log('Array!');
+// }else{
+//   console.log(typeof arrayList);
+// }
+
+
